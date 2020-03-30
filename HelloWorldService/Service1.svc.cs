c950ae03d17fb6031dc6cde55ad66e94106fa409
Brendan Anders <brendan.8thnote@gmail.com>
2020-03-30 16:07:32 -0700

    Activity Updates

---
+++ HelloWorldService/Service1.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HelloWorldService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        //3
+        public HelloObject GetModelObject(string id)
+        {
+            HelloObject helloObject = new HelloObject();
+
+            if (Int32.Parse(id) > 0)
+            {
+                helloObject.happyHello = true;
+                helloObject.helloMessage = "Great day. Couldn't be better eh?";
+            }
+            else
+            {
+                helloObject.happyHello = false;
+                helloObject.helloMessage = "Feeling very glum. I hope the sun will come out tomorrow :-(";
+            }
+
+            return helloObject;
+        }
+
+        //2
+        public string GetData(string value)
+        {
+            return "If your voice travels " + value + " feet, then the influence of your voice will cover " + Int32.Parse(value) * Int32.Parse(value) * 3.14 + " sq feet";
+        }
+
+        //1
+        public string SayHello()
+        {
+            return "Dear Friend, I pray that all may go well with you and that you may be in good health, as it goes well with your soul.";
+        }
+    }
+}
